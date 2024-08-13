1a1c78bd0bb9daf2c3b1c6d7e77988b9a93709f9
Likhapha <mohlabilikha@gmail.com>
2024-08-13 12:00:38 +0200

    my last commit

--- Tracer_Study/Tracer_Study/wsgi.py
+++ Tracer_Study/Tracer_Study/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tracer_Study.Tracer_Study.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tracer_Study.settings')
 
 application = get_wsgi_application()
