af668505d6dada373098fdeeb3bcd8e159e20dfd
Likhapha <mohlabilikha@gmail.com>
2024-08-13 11:52:09 +0200

    my last commit

--- Tracer_Study/manage.py
+++ Tracer_Study/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tracer_Study.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tracer_study.Tracer_Study.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
