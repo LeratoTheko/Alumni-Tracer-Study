bc223188b903c29e3d0da16c715068858c5ad2fc
Likhapha <103067413+Likhapha@users.noreply.github.com>
2024-09-19 15:27:57 +0200

    Update settings.py

--- Tracer_Study/Tracer_Study/settings.py
+++ Tracer_Study/Tracer_Study/settings.py
@@ -123,7 +123,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [BASE_DIR / "static"]
+#STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
